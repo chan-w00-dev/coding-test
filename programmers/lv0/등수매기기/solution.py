4e0213ae4ff0717bb3541cebe60bc24e9bbeaa3d
chan-w00-dev <pklucas1022@gmail.com>
2026-08-05 15:00:59 +0900

    refactor: 등수 매기기 - 컴프리헨션, sum() 제네레이터

--- programmers/lv0/등수매기기/solution.py
+++ programmers/lv0/등수매기기/solution.py
@@ -1,12 +1,6 @@
 def solution(score):
-    answer = [0 for i in range(len(score))]
     s = [sum(x) for x in score]
 
-    for i, n in enumerate(s):
-        rank = 1
-        for x in s:
-            if x > n:
-                rank += 1
-        answer[i] = rank
+    answer = [sum(x > n for x in s) + 1 for n in s]
 
     return answer
