76cdf30f6ad506488ed8c0ae9bde018404af9c35
chan-w00-dev <pklucas1022@gmail.com>
2026-08-18 14:18:56 +0900

    refactor: 두 개 뽑아서 더하기 - itertools.combinations, set()

--- programmers/lv1/두개뽑아서더하기/solution.py
+++ programmers/lv1/두개뽑아서더하기/solution.py
@@ -2,4 +2,4 @@ from itertools import combinations
 
 def solution(numbers):
     answer = [sum(c) for c in combinations(numbers,2)]
-    return sorted(list(dict.fromkeys(answer)))
+    return sorted(set(answer))
