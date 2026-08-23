12055456b35c955f3a9cd81f24aa375462590834
chan-w00-dev <pklucas1022@gmail.com>
2026-08-23 14:12:26 +0900

    solve: [1차] 다트 게임 (Lv1, 61%)

---
+++ programmers/lv1/[1차]다트게임/solution.py
@@ -0,0 +1,41 @@
+def solution(dartResult):   
+    num = [str(x) for x in range(0,11)]
+    bonus = ["S","D","T"]
+    option = ["*","#"]
+    idx = 0
+    score = [0] * 3
+
+
+    for i, x in enumerate(dartResult):
+        
+        if x == "1" and dartResult[i+1] == "0":
+            score[idx] = 10
+        elif x == "0" and dartResult[i-1] == "1":
+            continue
+        elif x in num:
+            score[idx] = int(x)
+
+        if x in bonus:
+            if x == "D":
+                score[idx] = score[idx] ** 2
+            elif x == "T":
+                score[idx] = score[idx] ** 3
+            idx += 1
+
+        if x in option:
+            if x == "*" and idx >= 2:
+                score[idx-2] *= 2
+                score[idx-1] *= 2
+                
+            elif x == "*" and idx == 1:
+                score[idx-1] *= 2
+
+            if x == "#":
+                score[idx-1] *= -1
+
+    return sum(score)
+
+            
+
+
+    
